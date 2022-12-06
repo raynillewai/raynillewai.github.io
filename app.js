6c1a99d2031239294c50218214384ef63b138801
raynillewai <51314869+raynillewai@users.noreply.github.com>
2022-12-06 12:49:50 +0800

    Update app.js

--- app.js
+++ app.js
@@ -67,5 +67,6 @@ function getMobileOperatingSystem() {
 */
 function goAppDeepLink() {
     // Deep link to your app goes here
-    window.location = "https://touchngoewallet.onelink.me/8mmV/streetparkingtng";
+    window.location = "https://lzd.co/1212LazzieHunt";
+//     window.location = "https://touchngoewallet.onelink.me/8mmV/streetparkingtng";
 }
