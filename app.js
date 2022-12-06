2f7048c4a31274f4acbe07c97575b44d067f2f8a
raynillewai <51314869+raynillewai@users.noreply.github.com>
2022-12-06 13:55:47 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,46 +1,33 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var deviceIsMobile = false;
 
-    // if device is mobile, set flag to true
-    if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
-        || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) {
-        deviceIsMobile = true;
-    }
+    if (getMobileOperatingSystem() == "Android") {
 
-    if(deviceIsMobile) {
-        
-        getMobileOperatingSystem();
-
-        if (getMobileOperatingSystem() == "Android") {
-
-            // console.log("android");
-            goAppDeepLink();
-
-            setTimeout(function() {
-                // Link to the App Store go here -- only fires if deep link fails   
-                window.location = "https://touchngoewallet.onelink.me/8mmV/playstore";
-            }, 500);
-        }
-        else if (getMobileOperatingSystem() == "iOS") {
-//             alert("ios");
-
-            goAppDeepLink();
-
-//             setTimeout(function() {
-//                 // Link to the App Store go here -- only fires if deep link fails     
-//                 window.location = "https://touchngoewallet.onelink.me/8mmV/appstore";
-//             }, 500);
-        }
-    }
-    // Fallback url, if user opens QR using non-mobile device, direct them to TnG website
-    else {
-        // console.log("web browser");
         setTimeout(function() {
-            window.location = "https://www.touchngo.com.my/";
-            
-        }, 500);
+            // Link to the App Store go here -- only fires if deep link fails   
+            // window.location.href = "https://touchngoewallet.onelink.me/8mmV/playstore";
+            window.location = "https://play.google.com/store/apps/details?id=my.com.tngdigital.ewallet&shortlink=playstore&pid=Webpage&af_click_lookback=7d";
+
+        }, 25);
+
+        goAppDeepLink();
+
+    } 
+    else if (getMobileOperatingSystem() == "iOS") {
+
+        setTimeout(function() {
+            // Link to the App Store go here -- only fires if deep link fails     
+            // window.location = "https://touchngoewallet.onelink.me/8mmV/appstore";
+            window.location = "https://apps.apple.com/my/app/touch-n-go-ewallet/id1344696702";
+        }, 25);
+
+        goAppDeepLink();
     }
-});
+    else {
+        x.innerHTML = "unknown";
+        goAppDeepLink();
+    }
+
+})
 
 function getMobileOperatingSystem() {
     var userAgent = navigator.userAgent || navigator.vendor || window.opera;
@@ -62,11 +49,7 @@ function getMobileOperatingSystem() {
     return "unknown";
 }
 
-/*
-    Go into TnG App's RFID mini program
-*/
 function goAppDeepLink() {
-    // Deep link to your app goes here
+    // Deep link to TnG app goes here
     window.location = "https://lzd.co/1212LazzieHunt";
-//     window.location = "https://touchngoewallet.onelink.me/8mmV/streetparkingtng";
 }
