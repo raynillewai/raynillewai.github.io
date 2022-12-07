9d2424f06e94162d5f4bd6c163fadabfd46562c8
raynillewai <51314869+raynillewai@users.noreply.github.com>
2022-12-07 10:47:22 +0800

    Update app.js

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ document.addEventListener("DOMContentLoaded", function() {
     }
     if (getMobileOperatingSystem() == "iOS") {
         
-        setTimeout(function() {
-            // Link to the App Store go here -- only fires if deep link fails     
-            window.location = "https://touchngoewallet.onelink.me/8mmV/appstore";
-        }, 3000);
+//         setTimeout(function() {
+//             // Link to the App Store go here -- only fires if deep link fails     
+//             window.location = "https://touchngoewallet.onelink.me/8mmV/appstore";
+//         }, 3000);
 
     }
 
